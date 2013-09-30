bd0508e235ed10bf4559f21a41618f8d03e33a46
Wesley Curtis <wtcurtis+github@gmail.com>
2013-09-30 01:10:10 +0000

    Add debounce interval to config

--- app/config.php
+++ app/config.php
@@ -6,6 +6,8 @@ return array(
         'consumer_secret' => '',
         'oauth_token' => '',
         'oauth_token_secret' => '',
+        // Minimum number of seconds between identical queries on Twitter
+        'min_query_interval' => 120
     ),
     'db' => array(
         'host' => '',
